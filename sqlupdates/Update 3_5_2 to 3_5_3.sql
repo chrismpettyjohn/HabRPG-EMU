e1a1678439632a842277dea1d6b527344fe75dd2
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-19 19:19:30 -0500

    Clean up

--- sqlupdates/Update 3_5_2 to 3_5_3.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('pathfinder.execution_time.milli', '25');
-INSERT INTO `emulator_settings`(`key`, `value`) VALUES ('pathfinder.max_execution_time.enabled', '1');
-
