92d9ae219c029d5c4b5226c9a4bb029148d65faa
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-01-20 18:15:21 -0500

    feat: lay when dead

--- src/main/java/com/eu/habbo/habbohotel/roleplay/character/events/CharacterDiedEvent.java
+++ src/main/java/com/eu/habbo/habbohotel/roleplay/character/events/CharacterDiedEvent.java
@@ -3,21 +3,29 @@ package com.eu.habbo.habbohotel.roleplay.character.events;
 import com.eu.habbo.Emulator;
 import com.eu.habbo.habbohotel.pets.PetVocal;
 import com.eu.habbo.habbohotel.roleplay.character.RoleplayCharacter;
+import com.eu.habbo.habbohotel.rooms.RoomUnitStatus;
+import com.eu.habbo.messages.outgoing.rooms.users.RoomUserStatusComposer;
 
 public class CharacterDiedEvent {
 
     public CharacterDiedEvent(RoleplayCharacter character) {
         if (character.getBot() != null) {
             character.getBot().shout(Emulator.getTexts().getValue("rp.died"));
+            character.getBot().getRoomUnit().setStatus(RoomUnitStatus.LAY, 0.5 + "");
+            character.getBot().getRoom().sendComposer(new RoomUserStatusComposer(character.getBot().getRoomUnit()).compose());
             return;
         }
 
         if (character.getHabbo() != null) {
             character.getHabbo().shout(Emulator.getTexts().getValue("rp.died"));
+            character.getHabbo().getRoomUnit().setStatus(RoomUnitStatus.LAY, 0.5 + "");
+            character.getHabbo().getRoomUnit().getRoom().sendComposer(new RoomUserStatusComposer(character.getHabbo().getRoomUnit()).compose());
         }
 
         if (character.getPet() != null) {
             character.getPet().say(new PetVocal(Emulator.getTexts().getValue("rp.died")));
+            character.getPet().getRoomUnit().setStatus(RoomUnitStatus.LAY, 0.5 + "");
+            character.getPet().getRoom().sendComposer(new RoomUserStatusComposer(character.getPet().getRoomUnit()).compose());
         }
     }
 
