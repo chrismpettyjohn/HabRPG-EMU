72fb8b57f9c2391953ab9f3c910faefaa89a5e1d
chrismpettyjohn <chrismpettyjohn@gmail.com>
2025-02-17 19:42:04 -0500

    feat: add username and figure to rp data

--- src/main/java/com/eu/habbo/messages/outgoing/roleplay/character/CharacterDataComposer.java
+++ src/main/java/com/eu/habbo/messages/outgoing/roleplay/character/CharacterDataComposer.java
@@ -14,11 +14,26 @@ public class CharacterDataComposer extends MessageComposer {
 
     @Override
     protected ServerMessage composeInternal() {
+
+        String username = this.character.getHabbo() != null
+                ? this.character.getHabbo().getHabboInfo().getUsername()
+                : this.character.getBot() != null
+                    ? this.character.getBot().getName()
+                    : this.character.getPet().getName();
+
+        String figure = this.character.getHabbo() != null
+                ? this.character.getHabbo().getHabboInfo().getLook()
+                : this.character.getBot() != null
+                ? this.character.getBot().getFigure()
+                : "-";
+
         this.response.init(Outgoing.CharacterDataComposer);
         this.response.appendInt(this.character.getId());
         this.response.appendInt(this.character.getBotId() == null ? -1 : this.character.getBotId());
         this.response.appendInt(this.character.getUserId() == null ? -1 : this.character.getUserId());
         this.response.appendInt(this.character.getPetId() == null ? -1 : this.character.getPetId());
+        this.response.appendString(username);
+        this.response.appendString(figure);
         this.response.appendBoolean(this.character.isDead());
         this.response.appendBoolean(this.character.isExhausted());
         this.response.appendBoolean(this.character.isWorking());
